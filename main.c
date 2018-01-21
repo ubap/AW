7f9ef8ff692504ca8fb7dda18b1d9800bae53b23
jakub trzebiatowski <jaktrzeb@gmail.com>
2018-01-21 14:56:06 +0100

    fix qdbmp

--- main.c
+++ main.c
@@ -4,21 +4,16 @@
 
 #include "qdbmp.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
 
 #define FILE_NAME "mario.bmp"
 
-void print_image(unsigned char *data, int x, int y, int n) {
+void print_image(BMP* bmp, int x, int y) {
 	int i, j, k;
 	for (i = 0; i < y; i++) {
 		for (j = 0; j < x; j++) {
-			for (k = 0; k < n; k++) {
-				unsigned char value = *(data + (i*x*n) + (j*n) + k);
-				printf("data[%d][%d][%d]=%d\n", i, j, k, value);
-			}
+			unsigned char r, g, b;
+			BMP_GetPixelRGB(bmp, j, i, &r, &g, &b);
+			printf("x:%d, y:%d, r:%d, g:%d, b:%d\n", j, i, r, g, b);
 		}
 	}
 }
@@ -31,27 +26,32 @@ void resize_nearest_neighbour(unsigned char *original_data, int x,
 		for (j = 0; j < x; j++) {
 			for (k = 0; k < factor; k++) {
 				// powielenie pixeli w wierszu
-				memcpy(result_data + (factor*i*x*n*factor) + (factor*j*n) + (k*n),
-						original_data + (i*x*n) + (j*n), n);
+//				memcpy(result_data + (factor*i*x*n*factor) + (factor*j*n) + (k*n),
+//						original_data + (i*x*n) + (j*n), n);
 			}
 		}
 		for (k = 0; k < factor; k++) {
 			// powielenie wierszy
-			memcpy(result_data + (factor*i*x*n*factor) + (x*n*factor*k),
-					result_data + (factor*i*x*n*factor), x*n*factor);
+//			memcpy(result_data + (factor*i*x*n*factor) + (x*n*factor*k),
+//					result_data + (factor*i*x*n*factor), x*n*factor);
 		}
 	}
 }
 
-unsigned char* get_pixel(unsigned char* data, int height, int width,
-		int channels, int x, int y) {
+unsigned char* get_pixel(BMP* bmp, int x, int y,
+		unsigned char* r, unsigned char* g, unsigned char* b) {
+
+	int width = BMP_GetWidth(bmp);
+	int height = BMP_GetHeight(bmp);
+	
 	if (x < 0) x = 0;
 	if (x >= width) x = width - 1;
 	
 	if (y < 0) y = 0;
 	if (y >= height) y = height - 1;
 	
-	return data + (y*width*channels) + (x*channels);
+	BMP_GetPixelRGB(bmp, x, y, r, g, b);
+	//printf("get_pixel x:%d, y:%d, r:%d, g:%d, b:%d\n", x, y, *r, *g, *b);
 }
 
 float cubic_hermite(float A, float B, float C, float D, float t)
@@ -64,8 +64,12 @@ float cubic_hermite(float A, float B, float C, float D, float t)
     return a*t*t*t + b*t*t + c*t + d;
 }
 
-void bicubic_sample(unsigned char* data, int height, int width,
-		int channels, unsigned char* dest_pixel, float u, float v) {
+void bicubic_sample(BMP* original_bitmap, float u, float v,
+		unsigned char* r, unsigned char* g, unsigned char* b) {
+	
+	int width = BMP_GetWidth(original_bitmap);
+	int height = BMP_GetHeight(original_bitmap);
+	
 	float x = (u * width) - 0.5; if (x<0) x = 0;
 	int xint = (int)x;
 	float xfract = x - floor(x);
@@ -75,29 +79,51 @@ void bicubic_sample(unsigned char* data, int height, int width,
 	float yfract = y - floor(y);
 	
 	//printf("x: %f, xint: %d, y: %f, yint: %d\n", x, xint, y, yint);
+
+	// r
+	unsigned char p00[3]; 
+	unsigned char p10[3];
+	unsigned char p20[3];
+	unsigned char p30[3];
 	
-	unsigned char* p00 = get_pixel(data, height, width, channels, xint - 1, yint - 1);
-	unsigned char* p10 = get_pixel(data, height, width, channels, xint + 0, yint - 1);
-	unsigned char* p20 = get_pixel(data, height, width, channels, xint + 1, yint - 1);
-	unsigned char* p30 = get_pixel(data, height, width, channels, xint + 2, yint - 1);
+	unsigned char p01[3];
+	unsigned char p11[3];
+	unsigned char p21[3];
+	unsigned char p31[3];
 	
-	unsigned char* p01 = get_pixel(data, height, width, channels, xint - 1, yint + 0);
-	unsigned char* p11 = get_pixel(data, height, width, channels, xint + 0, yint + 0);
-	unsigned char* p21 = get_pixel(data, height, width, channels, xint + 1, yint + 0);
-	unsigned char* p31 = get_pixel(data, height, width, channels, xint + 2, yint + 0);
+	unsigned char p02[3];
+	unsigned char p12[3];
+	unsigned char p22[3];
+	unsigned char p32[3];
 	
-	unsigned char* p02 = get_pixel(data, height, width, channels, xint - 1, yint + 1);
-	unsigned char* p12 = get_pixel(data, height, width, channels, xint + 0, yint + 1);
-	unsigned char* p22 = get_pixel(data, height, width, channels, xint + 1, yint + 1);
-	unsigned char* p32 = get_pixel(data, height, width, channels, xint + 2, yint + 1);
+	unsigned char p03[3];
+	unsigned char p13[3];
+	unsigned char p23[3];
+	unsigned char p33[3];
 	
-	unsigned char* p03 = get_pixel(data, height, width, channels, xint - 1, yint + 2);
-	unsigned char* p13 = get_pixel(data, height, width, channels, xint + 0, yint + 2);
-	unsigned char* p23 = get_pixel(data, height, width, channels, xint + 1, yint + 2);
-	unsigned char* p33 = get_pixel(data, height, width, channels, xint + 2, yint + 2);
+	get_pixel(original_bitmap, xint - 1, yint - 1, &p00[0], &p00[1], &p00[2]);
+	get_pixel(original_bitmap, xint + 0, yint - 1, &p10[0], &p10[1], &p10[2]);
+	get_pixel(original_bitmap, xint + 1, yint - 1, &p20[0], &p20[1], &p20[2]);
+	get_pixel(original_bitmap, xint + 2, yint - 1, &p30[0], &p30[1], &p30[2]);
 	
+	get_pixel(original_bitmap, xint - 1, yint + 0, &p01[0], &p01[1], &p01[2]);
+	get_pixel(original_bitmap, xint + 0, yint + 0, &p11[0], &p11[1], &p11[2]);
+	get_pixel(original_bitmap, xint + 1, yint + 0, &p21[0], &p21[1], &p21[2]);
+	get_pixel(original_bitmap, xint + 2, yint + 0, &p31[0], &p31[1], &p31[2]);
+	
+	get_pixel(original_bitmap, xint - 1, yint + 1, &p02[0], &p02[1], &p02[2]);
+	get_pixel(original_bitmap, xint + 0, yint + 1, &p12[0], &p12[1], &p12[2]);
+	get_pixel(original_bitmap, xint + 1, yint + 1, &p22[0], &p22[1], &p22[2]);
+	get_pixel(original_bitmap, xint + 2, yint + 1, &p32[0], &p32[1], &p32[2]);
+	
+	get_pixel(original_bitmap, xint - 1, yint + 2, &p03[0], &p03[1], &p03[2]);
+	get_pixel(original_bitmap, xint + 0, yint + 2, &p13[0], &p13[1], &p13[2]);
+	get_pixel(original_bitmap, xint + 1, yint + 2, &p23[0], &p23[1], &p23[2]);
+	get_pixel(original_bitmap, xint + 2, yint + 2, &p33[0], &p33[1], &p33[2]);
+	
+	unsigned char dest_pixel[3];
 	int channel;
-	for (channel = 0; channel < channels; channel++) {
+	for (channel = 0; channel < 3 /*RGB*/; channel++) {
 		float col0 = cubic_hermite(p00[channel], p10[channel], p20[channel], p30[channel], xfract);
 		float col1 = cubic_hermite(p01[channel], p11[channel], p21[channel], p31[channel], xfract);
 		float col2 = cubic_hermite(p02[channel], p12[channel], p22[channel], p32[channel], xfract);
@@ -107,25 +133,30 @@ void bicubic_sample(unsigned char* data, int height, int width,
 		if (value < 0.0f) value = 0.0f;
 		dest_pixel[channel] = (unsigned char)value;
 	}
+	*r = dest_pixel[0];
+	*g = dest_pixel[1];
+	*b = dest_pixel[2];
 }
 
-void resize_image(unsigned char* original_data,
-		int original_width, int original_height,
-		int channels, int factor, unsigned char *result_data) {
+void resize_image(BMP* original_bitmap, int factor, BMP* result_bitmap) {
 	
+	int original_height = BMP_GetHeight(original_bitmap);
+	int original_width = BMP_GetWidth(original_bitmap);
 	int desired_height = original_height * factor;
 	int desired_width = original_width * factor;
 
 	int y, x;
-#pragma omp parallel for private(x)
 	for (y = 0; y < desired_height; y++) {
 		float v = (float)y / (float)(desired_height - 1);
 		for (x = 0; x < desired_width; x++) {
 			float u = (float)x / (float)(desired_width - 1);
-			unsigned char *dest_pixel = result_data + y*desired_width*channels + x*channels;
 			
-			bicubic_sample(original_data, original_height, original_width, channels,
-					dest_pixel, u, v);
+			unsigned char r, g, b;
+			bicubic_sample(original_bitmap, u, v, &r, &g, &b);
+			
+			//printf("x: %d, y:%d, r:%d, g:%d, b:%d\n", x, y, r, g, b);
+			
+			BMP_SetPixelRGB(result_bitmap, x, y, r, g, b);
 		}
 	}
 }
@@ -140,42 +171,28 @@ int main(int argc, char* argv[]) {
 		printf("podaj prawidlowa skale\n");
 		return 5;
 	}
-	
-	int x,y,n;
-	unsigned char *data = stbi_load(FILE_NAME, &x, &y, &n, 0);
-	
-	if (data == NULL) {
-		printf("blad wczytywania pliku");
-		return 1;
-	}
-	printf("Szerokosc: %d, wysokosc: %d, liczba kanalow: %d\n", x, y, n);
-	//print_image(data, x, y, n);
 
-	BMP* bmp;
-	
-	bmp = BMP_ReadFile(FILE_NAME);
+	BMP* bmp = BMP_ReadFile(FILE_NAME);
 	 /* If an error has occurred, notify and exit */
 	BMP_CHECK_ERROR( stderr, -1 );
 	
-	int width = BMP_GetWidth( bmp );
-	int height = BMP_GetHeight( bmp );
+	int width = BMP_GetWidth(bmp);
+	int height = BMP_GetHeight(bmp);
 	int depth = BMP_GetDepth(bmp);
+	//print_image(bmp, width, height);
 	
-	BMP* resized_img2 = BMP_Create( width, height, depth );
-	BMP_WriteFile( resized_img2, "mario-2-qdbmp.bmp" );
+	printf("Szerokosc: %d, wysokosc: %d, liczba kanalow: %d\n", width, height, depth);
+	
+	BMP* resized_img2 = BMP_Create( width*factor, height*factor, 32 );
+	resize_image(bmp, factor, resized_img2);
+	
+	BMP_WriteFile( resized_img2, "output-mario.bmp" );
 	BMP_CHECK_ERROR( stderr, -2 );
 	
-	
-	unsigned char *resized_img = calloc(1, x*factor*y*factor*n);
+	BMP_Free(bmp);
+	BMP_Free(resized_img2);
 	
 	//resize_nearest_neighbour(data, x, y, n, factor, resized_img);
-	
-	resize_image(data, x, y, n, factor, resized_img);
-
-	stbi_write_bmp("mario1-output.bmp", factor*x, factor*y, n, resized_img);
-	
-	free(resized_img);
-	stbi_image_free(data);
 	
 	return 0;
 }
