6b6e0aea60d2b26444096d0ad23f57189093f177
jakub trzebiatowski <jaktrzeb@gmail.com>
2018-01-21 17:49:50 +0100

    work2

--- main.c
+++ main.c
@@ -122,7 +122,6 @@ void bicubic_sample(BMP* original_bitmap, float u, float v,
 	
 	unsigned char dest_pixel[3];
 	int channel;
-#pragma simd
 	for (channel = 0; channel < 3 /*RGB*/; channel++) {
 		float col0 = cubic_hermite(p00[channel], p10[channel], p20[channel], p30[channel], xfract);
 		float col1 = cubic_hermite(p01[channel], p11[channel], p21[channel], p31[channel], xfract);
@@ -144,20 +143,22 @@ void resize_image(BMP* original_bitmap, int factor, BMP* result_bitmap) {
 	int original_width = BMP_GetWidth(original_bitmap);
 	int desired_height = original_height * factor;
 	int desired_width = original_width * factor;
-
-	int y;
-	for (y = 0; y < desired_height; y++) {
-		float v = (float)y / (float)(desired_height - 1);
-		int x;
-		for (x = 0; x < desired_width; x++) {
-			float u = (float)x / (float)(desired_width - 1);
-			
-			unsigned char r, g, b;
-			bicubic_sample(original_bitmap, u, v, &r, &g, &b);
-			
-			//printf("x: %d, y:%d, r:%d, g:%d, b:%d\n", x, y, r, g, b);
-			
-			BMP_SetPixelRGB(result_bitmap, x, y, r, g, b);
+	{
+		int y;
+#pragma omp parallel for
+		for (y = 0; y < desired_height; y++) {
+			float v = (float)y / (float)(desired_height - 1);
+			int x;
+			for (x = 0; x < desired_width; x++) {
+				float u = (float)x / (float)(desired_width - 1);
+				
+				unsigned char r, g, b;
+				bicubic_sample(original_bitmap, u, v, &r, &g, &b);
+				
+				//printf("x: %d, y:%d, r:%d, g:%d, b:%d\n", x, y, r, g, b);
+				
+				BMP_SetPixelRGB(result_bitmap, x, y, r, g, b);
+			}
 		}
 	}
 }
