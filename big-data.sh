b24906ba6f08952d1ce789436908bee961046295
jakub trzebiatowski <jaktrzeb@gmail.com>
2018-01-25 23:15:38 +0100

    fix big data

--- big-data.sh
+++ big-data.sh
@@ -17,3 +17,8 @@ time ./a.out windows_xp_bliss-1600x900.bmp 4
 time ./a.out windows_xp_bliss-1600x900.bmp 4
 time ./a.out windows_xp_bliss-1600x900.bmp 4
 time ./a.out windows_xp_bliss-1600x900.bmp 4
+
+time ./a.out windows_xp_bliss-1600x900.bmp 4
+time ./a.out windows_xp_bliss-1600x900.bmp 4
+time ./a.out windows_xp_bliss-1600x900.bmp 4
+time ./a.out windows_xp_bliss-1600x900.bmp 4
